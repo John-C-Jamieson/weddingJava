1f0ea45aca7c9e92a00dacc2a6334333be6cdc84
John Jamieson <john@johnjamieson.ca>
2025-02-05 13:19:27 -0500

    Setup .env and initial database work

--- src/main/java/ca/johnjamieson/weddingjava/WeddingJavaApplication.java
+++ src/main/java/ca/johnjamieson/weddingjava/WeddingJavaApplication.java
@@ -1,13 +1,19 @@
 package ca.johnjamieson.weddingjava;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeddingJavaApplication {
+public class WeddingJavaApplication
+{
+	public static void main(String[] args)
+	{
+		// Load the .env file
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
-	public static void main(String[] args) {
+		// Set each .env variable as a system property
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(WeddingJavaApplication.class, args);
 	}
-
 }
