411a5ca8b009b57e9c408f6581b77f1c2099d5e8
John Jamieson <john@johnjamieson.ca>
2025-02-06 09:48:15 -0500

    Replaced java-dotenv with spring-dotenv

--- src/main/java/ca/johnjamieson/weddingjava/WeddingJavaApplication.java
+++ src/main/java/ca/johnjamieson/weddingjava/WeddingJavaApplication.java
@@ -1,6 +1,6 @@
 package ca.johnjamieson.weddingjava;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,16 +9,6 @@ public class WeddingJavaApplication
 {
 	public static void main(String[] args)
 	{
-		// Load the .env file
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-		// Set each .env variable as a system property
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
-		System.out.println("DB_URL: " + System.getProperty("DB_URL"));
-		System.out.println("DB_USERNAME: " + System.getProperty("DB_USERNAME"));
-		System.out.println("DB_PASSWORD: " + System.getProperty("DB_PASSWORD"));
-
 		SpringApplication.run(WeddingJavaApplication.class, args);
 	}
 }
