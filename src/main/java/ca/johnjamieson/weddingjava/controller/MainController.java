a84f9aed0a91869ebb56351a5396c2d9d2f10f47
John Jamieson <john@johnjamieson.ca>
2025-02-10 14:45:55 -0500

    Added page links

--- src/main/java/ca/johnjamieson/weddingjava/controller/MainController.java
+++ src/main/java/ca/johnjamieson/weddingjava/controller/MainController.java
@@ -5,14 +5,41 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController extends BaseController {
+public class MainController extends BaseController
+{
     @GetMapping("/")
-    public String showSaveTheDatePage(Model model) {
+    public String showSaveTheDatePage(Model model)
+    {
         return "index"; // returns the view name (index.html)
     }
 
     @GetMapping("/location")
-    public String showLocation(Model model) {
+    public String showLocation(Model model)
+    {
         return "location";// returns the view name (location.html)
+    }
+
+    @GetMapping("/dress")
+    public String showDress(Model model)
+    {
+        return "dress";// returns the view name (location.html)
+    }
+
+    @GetMapping("/menu")
+    public String showMenu(Model model)
+    {
+        return "menu";// returns the view name (location.html)
+    }
+
+    @GetMapping("/registry")
+    public String showRegistry(Model model)
+    {
+        return "registry";// returns the view name (location.html)
+    }
+
+    @GetMapping("/schedule")
+    public String showSchedule(Model model)
+    {
+        return "schedule";// returns the view name (location.html)
     }
 }
